3a7612925905f129c247f4c139aa0b896499346d
Stéphane Wirtel <stephane@wirtel.be>
2021-05-26 14:20:53 +0200

    Use a single string for the author

--- dsmtpd/__init__.py
+++ dsmtpd/__init__.py
@@ -7,6 +7,6 @@
     :license: BSD, see LICENSE for more details
 """
 __name__ = "dsmtpd"
-__version__ = "0.3git"
-__author__ = ("Stephane Wirtel",)
+__version__ = "0.3"
+__author__ = "Stephane Wirtel"
 __author_email__ = "stephane@wirtel.be"
